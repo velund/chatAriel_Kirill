8156452635095ff329702ed44623a25279eaacaa
h67.ariel <arikkle123@gmail.com>
2021-04-28 22:03:20 +0300

    getting somewhere

--- client/chatOpener.c
+++ client/chatOpener.c
@@ -1,20 +1,26 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
-int openChat(char  *_grpIp, int _grpPort)
+
+
+int openChat(char  *_grpIp, int _grpPort, char* _userName, char* _groupName)
 {
-    
     char listener[100];
     char sender[100];
-    sprintf(listener, "mate-terminal --geometry 20x20+500+250 --command ./listen.out 224.0.0.1 8080");
-    sprintf(sender, "mate-terminal --geometry 10x10+500+500 --command ./send.out %s %d", _grpIp, _grpPort);
+    sprintf(listener, "gnome-terminal -- ./listen.out %s %d %s", _grpIp, _grpPort, _groupName);
+    sprintf(sender, "gnome-terminal -- ./send.out %s %d %s %s", _grpIp, _grpPort, _userName, _groupName);
+    
     system(listener);
     system(sender);
     return 0;
 }
+
+
 int main(int argc, char const *argv[])
 {
-    openChat("224.0.0.1", 8080);
+    openChat("224.0.0.1", 8080, "Arik", "Group1");
+
+    openChat("224.0.0.1", 8080, "Kirill", "Group1");
     
     return 0;
 }
---
+++ client/sender.c
@@ -0,0 +1,97 @@
+#include <sys/socket.h> /* socket() */
+#include <netinet/in.h> /* struct sockaddr_in */
+#include <arpa/inet.h> /* inet_ntoa() */
+#include <stdio.h> /* scanf(), perror() */
+#include <stdlib.h> /* exit() */
+#include <string.h> /* memset() */
+
+#define BUFFER_SIZE 100
+
+/* HELPER */
+static int GetSock();
+static struct sockaddr_in InitComm(int _sock, int _port, char* _group);
+
+void SenderRun(int _socket, struct sockaddr_in _addr, char* _userName, char* _groupName);
+
+
+/* MAIN */
+
+int main(int argc, char *argv[])
+{
+    int sock, port;
+    char* groupAddr, *userName, *groupName;
+    struct sockaddr_in addr;
+
+    if (argc != 5) 
+    {
+       perror("Args fail");
+       exit(1);
+    }
+
+    groupAddr = argv[1];
+    sscanf(argv[2], "%d", &port);  
+    userName = argv[3];
+    groupName = argv[4];
+
+    sock = GetSock();
+    addr = InitComm(sock, port, groupAddr);
+
+    SenderRun(sock, addr, userName, groupName);
+    
+
+    return 0;
+}
+
+
+/* ------------------------ HELPER ------------------------ */ 
+
+
+static int GetSock()
+{
+    int sock;
+    sock = socket(AF_INET, SOCK_DGRAM, 0);
+    if (sock < 0) 
+    {
+        perror("socket get err");
+        exit(1);
+    }
+}
+
+static struct sockaddr_in InitComm(int _sock, int _port, char* _group)
+{
+    struct sockaddr_in addr;
+
+    memset(&addr, 0, sizeof(addr));
+    addr.sin_family = AF_INET;
+    addr.sin_addr.s_addr = inet_addr(_group);
+    addr.sin_port = htons(_port);
+
+    return addr;
+}
+
+
+
+void SenderRun(int _socket, struct sockaddr_in _addr, char* _userName, char* _groupName)
+{
+    int sentBytes;
+    char buffer[BUFFER_SIZE];
+    char finalMsg[BUFFER_SIZE];
+
+    printf("Welcome to group: %s!\n", _groupName);
+    printf("Please write any message:\n\n\n");
+
+    while (1) 
+    {
+        sprintf(finalMsg, "%s: ", _userName);
+        fgets(buffer, BUFFER_SIZE, stdin);
+        strcat(finalMsg, buffer);
+        
+        sentBytes = sendto(_socket, finalMsg, strlen(finalMsg), 0, (struct sockaddr*) &_addr, sizeof(_addr) );
+            
+        if (sentBytes < 0) 
+        {
+            perror("sendto");
+            exit(1);
+        }
+    }
+}
